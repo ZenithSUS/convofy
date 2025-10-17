b44d1f6bbaf6abffedacfd838f9b56a5e13dcf88
ZenithSUS <jeranmerino147@gmail.com>
2025-10-17 23:22:05 +0800

    fix: fix lint error

--- app/(views)/chat/anonymous/pages/index.tsx
+++ app/(views)/chat/anonymous/pages/index.tsx
@@ -2,6 +2,8 @@ import { Session } from "next-auth";
 import AnonymousRoomHeader from "../components/anon-room-header";
 
 function AnonymousPageClient({ session }: { session: Session }) {
+  if (!session) return null;
+
   return (
     <div className="flex flex-1 flex-col gap-2">
       <AnonymousRoomHeader />

bde6ef819ec3edf49244484b7b7a3cfcf0e415e3
ZenithSUS <jeranmerino147@gmail.com>
2025-11-04 22:42:04 +0800

    fix: Handle session retrieval failure gracefully in AnonymousChatPage

--- app/(views)/chat/anonymous/[roomId]/page.tsx
+++ app/(views)/chat/anonymous/[roomId]/page.tsx
@@ -3,7 +3,7 @@ import { getServerSession, Session } from "next-auth";
 import AnonymousPageClient from "../pages/room";
 
 export default async function AnonymousChatPage() {
-  const session = await getServerSession(authOptions);
+  const session = await getServerSession(authOptions).catch(() => null);
 
   return <AnonymousPageClient session={session as Session} />;
 }

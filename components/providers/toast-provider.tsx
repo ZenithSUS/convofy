16b6b495321f9c17b224e854fb5701aa0e92d330
ZenithSUS <jeranmerino147@gmail.com>
2025-11-08 16:58:16 +0800

    add: increase limit in the toast component and auto closes in 3 seconds.

--- components/providers/toast-provider.tsx
+++ components/providers/toast-provider.tsx
@@ -3,5 +3,12 @@
 import { ToastContainer } from "react-toastify";
 
 export const ToastProvider = () => {
-  return <ToastContainer limit={1} />;
+  return (
+    <ToastContainer
+      position="top-right"
+      autoClose={3000}
+      limit={3}
+      newestOnTop
+    />
+  );
 };

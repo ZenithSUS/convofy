3a2ea7e8b7994c5e810c853f9291343043d67e60
ZenithSUS <jeranmerino147@gmail.com>
2025-11-11 16:04:57 +0800

    modify: Tailwind suggested gradient to linear style background color.

--- app/(views)/chat/[roomId]/components/start-message.tsx
+++ app/(views)/chat/[roomId]/components/start-message.tsx
@@ -5,7 +5,7 @@ import { Send } from "lucide-react";
 function StartMessage() {
   return (
     <div className="flex flex-col items-center justify-center px-4 py-20">
-      <div className="mb-4 flex h-24 w-24 items-center justify-center rounded-full bg-gradient-to-br from-blue-100 to-purple-100">
+      <div className="mb-4 flex h-24 w-24 items-center justify-center rounded-full bg-linear-to-br from-blue-100 to-purple-100">
         <Send size={40} className="text-blue-500" />
       </div>
       <h3 className="mb-2 text-xl font-semibold text-gray-700">

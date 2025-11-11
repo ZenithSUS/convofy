d8ab017daf4af50bad26f5a082126a5817dcaa28
ZenithSUS <jeranmerino147@gmail.com>
2025-11-11 16:07:10 +0800

    feat: Change the behavior of showing toast into single one instead of 3 at the same time.

--- components/providers/toast-provider.tsx
+++ components/providers/toast-provider.tsx
@@ -1,13 +1,26 @@
 "use client";
 
-import { ToastContainer } from "react-toastify";
+import { toast, ToastContainer, TypeOptions } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const TOAST_ID = "latest-toast";
+
+export const Toast = {
+  show: (message: string, type: TypeOptions = "default") => {
+    toast(message, { toastId: TOAST_ID, type });
+  },
+  success: (message: string) => Toast.show(message, "success"),
+  error: (message: string) => Toast.show(message, "error"),
+  info: (message: string) => Toast.show(message, "info"),
+  warn: (message: string) => Toast.show(message, "warning"),
+};
 
 export const ToastProvider = () => {
   return (
     <ToastContainer
       position="top-right"
       autoClose={3000}
-      limit={3}
+      limit={1}
       newestOnTop
     />
   );

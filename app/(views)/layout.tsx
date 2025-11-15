c93d68f9116fab50a984aea6691356c5695a5d6e
ZenithSUS <jeranmerino147@gmail.com>
2025-11-15 22:21:10 +0800

    refactor: Move the components and SEO to the root folder app layout.

--- app/(views)/layout.tsx
+++ app/(views)/layout.tsx
@@ -1,51 +1,7 @@
-import "@/styles/globals.css";
-import type { Metadata } from "next";
-import { Analytics } from "@vercel/analytics/next";
-import { SpeedInsights } from "@vercel/speed-insights/next";
-import { Geist, Geist_Mono } from "next/font/google";
-import AuthProvider from "@/components/providers/session-provider";
-import QueryProvider from "@/components/providers/query-provider";
-import { ToastProvider } from "@/components/providers/toast-provider";
-import GlobalPusherProvider from "@/components/providers/global-pusher-provider";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "Convofy",
-  description: "A platform to manage and share your conversations easily.",
-  icons: {
-    icon: "/convofy.png",
-  },
-};
-
-export default function RootLayout({
+export default function ViewsLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <QueryProvider>
-          <AuthProvider>
-            <GlobalPusherProvider />
-            <main>{children}</main>
-            <ToastProvider />
-          </AuthProvider>
-        </QueryProvider>
-        <Analytics />
-        <SpeedInsights />
-      </body>
-    </html>
-  );
+  return children;
 }

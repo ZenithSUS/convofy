096c0932fd1209fcedd9f3d6c814235c48bc6e10
ZenithSUS <jeranmerino147@gmail.com>
2025-11-15 22:43:25 +0800

    add: RoomRequest types.
    refactor: Create a lastMessage type to shorten the types related.

--- types/room.ts
+++ types/room.ts
@@ -1,20 +1,30 @@
 import { Types } from "mongoose";
 
+export type LastRoomMessage = {
+  _id: Types.ObjectId | string;
+  room: Types.ObjectId | string;
+  sender: Types.ObjectId | string;
+  content: string;
+  type: "text" | "image" | "file";
+  createdAt: Date;
+};
+
 export type Room = {
   _id: string;
   name: string;
   description: string;
   image?: string;
   members: string[];
-  lastMessage?: {
-    _id: Types.ObjectId | string;
-    room: Types.ObjectId | string;
-    sender: Types.ObjectId | string;
-    content: string;
-    type: "text" | "image" | "file";
-    createdAt: Date;
-  };
+  lastMessage?: LastRoomMessage;
   isPrivate: boolean;
+  isAccepted?: boolean;
+  isPending?: boolean;
+  invitedBy?: {
+    _id: string;
+    name: string;
+    avatar: string;
+  };
+  invitedUser?: string;
   owner: string;
   createdAt: Date;
 };
@@ -30,6 +40,18 @@ export type RoomContent = Omit<Room, "members"> & {
   members: RoomMembers[];
   type: "room" | "user";
   avatar?: string;
+};
+
+export type RoomRequest = Omit<
+  Room,
+  | "description"
+  | "members"
+  | "owner"
+  | "isAccepted"
+  | "isPending"
+  | "lastMessage"
+> & {
+  lastMessage: LastRoomMessage;
 };
 
 export type CreateRoom = Omit<Room, "_id">;

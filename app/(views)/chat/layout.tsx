3a5cbb37de86b2e9a31ed436a0c77d1594dbb92f
ZenithSUS <jeranmerino147@gmail.com>
2025-11-16 14:00:39 +0800

    add: A conditional block if there is no session.

--- app/(views)/chat/layout.tsx
+++ app/(views)/chat/layout.tsx
@@ -2,6 +2,7 @@ import ChatWrapper from "./components/chat-wrapper";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 import { Session } from "@/app/(views)/chat/components/chat-header";
+import { redirect } from "next/navigation";
 
 export default async function ChatLayout({
   children,
@@ -9,6 +10,11 @@ export default async function ChatLayout({
   children: React.ReactNode;
 }>) {
   const session = await getServerSession(authOptions).catch(() => null);
+
+  if (!session) {
+    // Redirect to login if no session
+    redirect("/auth/login");
+  }
 
   return (
     <ChatWrapper serverSession={session as Session}>{children}</ChatWrapper>

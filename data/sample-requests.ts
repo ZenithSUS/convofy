1d504f6d4f066d1799f4e087d3f74dee3e75b9c2
ZenithSUS <jeranmerino147@gmail.com>
2025-11-22 21:49:29 +0800

    Add isAnonymous and role fields to room and member data structures

--- data/sample-requests.ts
+++ data/sample-requests.ts
@@ -6,12 +6,14 @@ export const sampleMessageRequests: RoomRequest[] = [
     name: "Chat with Sarah",
     image: "https://i.pravatar.cc/150?img=1",
     isPrivate: true,
+    isAnonymous: false,
     invitedBy: {
       _id: "user_101",
       name: "Sarah Johnson",
       avatar: "https://i.pravatar.cc/150?img=1",
     },
     invitedUser: "user_current",
+    createdAt: new Date(Date.now() - 1000 * 60 * 5), // 5 minutes ago
     lastMessage: {
       _id: "msg_001",
       room: "req_001",
@@ -26,19 +28,20 @@ export const sampleMessageRequests: RoomRequest[] = [
       },
       isEdited: false,
     },
-    createdAt: new Date(Date.now() - 1000 * 60 * 5),
   },
   {
     _id: "req_002",
     name: "Chat with Michael",
     image: "https://i.pravatar.cc/150?img=12",
     isPrivate: true,
+    isAnonymous: false,
     invitedBy: {
       _id: "user_102",
       name: "Michael Chen",
       avatar: "https://i.pravatar.cc/150?img=12",
     },
     invitedUser: "user_current",
+    createdAt: new Date(Date.now() - 1000 * 60 * 30), // 30 minutes ago
     lastMessage: {
       _id: "msg_002",
       room: "req_002",
@@ -52,19 +55,20 @@ export const sampleMessageRequests: RoomRequest[] = [
       },
       isEdited: false,
     },
-    createdAt: new Date(Date.now() - 1000 * 60 * 30),
   },
   {
     _id: "req_003",
     name: "Chat with Emily",
     image: "https://i.pravatar.cc/150?img=5",
     isPrivate: true,
+    isAnonymous: false,
     invitedBy: {
       _id: "user_103",
       name: "Emily Rodriguez",
       avatar: "https://i.pravatar.cc/150?img=5",
     },
     invitedUser: "user_current",
+    createdAt: new Date(Date.now() - 1000 * 60 * 60 * 2), // 2 hours ago
     lastMessage: {
       _id: "msg_003",
       room: "req_003",
@@ -79,19 +83,20 @@ export const sampleMessageRequests: RoomRequest[] = [
       },
       isEdited: false,
     },
-    createdAt: new Date(Date.now() - 1000 * 60 * 60 * 2),
   },
   {
     _id: "req_004",
     name: "Chat with David",
     image: "https://i.pravatar.cc/150?img=15",
     isPrivate: true,
+    isAnonymous: false,
     invitedBy: {
       _id: "user_104",
       name: "David Park",
       avatar: "https://i.pravatar.cc/150?img=15",
     },
     invitedUser: "user_current",
+    createdAt: new Date(Date.now() - 1000 * 60 * 60 * 5), // 5 hours ago
     lastMessage: {
       _id: "msg_004",
       room: "req_004",
@@ -105,19 +110,20 @@ export const sampleMessageRequests: RoomRequest[] = [
       },
       isEdited: false,
     },
-    createdAt: new Date(Date.now() - 1000 * 60 * 60 * 5),
   },
   {
     _id: "req_005",
     name: "Chat with Jessica",
     image: "https://i.pravatar.cc/150?img=9",
     isPrivate: true,
+    isAnonymous: false,
     invitedBy: {
       _id: "user_105",
       name: "Jessica Williams",
       avatar: "https://i.pravatar.cc/150?img=9",
     },
     invitedUser: "user_current",
+    createdAt: new Date(Date.now() - 1000 * 60 * 60 * 24), // 1 day ago
     lastMessage: {
       _id: "msg_005",
       room: "req_005",
@@ -131,19 +137,20 @@ export const sampleMessageRequests: RoomRequest[] = [
       },
       isEdited: false,
     },
-    createdAt: new Date(Date.now() - 1000 * 60 * 60 * 24),
   },
   {
     _id: "req_006",
     name: "Chat with Alex",
     image: "https://i.pravatar.cc/150?img=33",
     isPrivate: true,
+    isAnonymous: false,
     invitedBy: {
       _id: "user_106",
       name: "Alex Thompson",
       avatar: "https://i.pravatar.cc/150?img=33",
     },
     invitedUser: "user_current",
+    createdAt: new Date(Date.now() - 1000 * 60 * 60 * 24 * 2), // 2 days ago
     lastMessage: {
       _id: "msg_006",
       room: "req_006",
@@ -157,7 +164,6 @@ export const sampleMessageRequests: RoomRequest[] = [
       },
       isEdited: false,
     },
-    createdAt: new Date(Date.now() - 1000 * 60 * 60 * 24 * 2),
   },
 ];
 
